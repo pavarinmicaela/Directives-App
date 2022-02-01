dceaf06f593bbc05939ecab83589ef8a6fb0f5a2
Maria Micaela PAVARIN <MMPAVARIN@suppliers.tenaris.com>
2022-02-01 18:00:17 -0300

    Structural directive - customIf

---
+++ src/app/shared/directives/custom-if.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[customIf]'
+})
+export class CustomIfDirective {
+
+  @Input() set customIf(show: boolean) {
+    if (show) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
+
+  constructor(
+    private templateRef: TemplateRef<HTMLElement>,
+    private viewContainer: ViewContainerRef
+  ) { }
+}
